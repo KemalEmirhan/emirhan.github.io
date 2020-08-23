243a675dcdb8c20ca0dcf16a09553442ddbc975d
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2020-08-23 20:56:03 +0300

    feat: add file list component

--- components/fileList.js
+++ components/fileList.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import styles from '../styles/fileList.module.scss';
 
 const FileList = () => {
     return (
-        <section>
-            <h3>File List Area</h3>
+        <section className={styles.list}>
+            <div className={styles.list_header}>
+                <h4>More informations about me</h4>
+            </div>
+            <ul className={styles.list_content}>
+                <li className={styles.list_content__item}>Github</li>
+                <li className={styles.list_content__item}>LinkedIn</li>
+                <li className={styles.list_content__item}>Medium</li>
+                <li className={styles.list_content__item}>Twitter</li>
+                <li className={styles.list_content__item}>Instagram</li>
+                <li className={styles.list_content__item}>Gmail</li>
+            </ul>
         </section>
     )
 }
