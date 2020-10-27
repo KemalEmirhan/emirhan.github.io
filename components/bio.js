541329cf64cfa17659d95e36b2636d94b8b30cad
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2020-10-27 19:49:03 +0300

    chore: nextjs version bump and small changes

--- components/bio.js
+++ components/bio.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from "next/image";
 import styles from '../styles/bio.module.scss';
 
 const Bio = () => {
@@ -7,14 +8,14 @@ const Bio = () => {
     return (
         <section className={styles.bio}>
             <div className={styles.bio_pic}>
-                <img src="/pp.jpeg" alt="Emirhan Kemal Kösem" loading="lazy" /> 
+                <Image src="/pp.jpeg" alt="Emirhan Kemal Kösem" loading="lazy" width="240px" height="240px" /> 
             </div>
             <div className={styles.bio_desc}>
                 <h2 className={styles.bio_desc__header}>
                     Emirhan Kemal KÖSEM
                 </h2>
                 <p className={styles.bio_desc__content}>
-                    I'm {age} years old. I graduated from my university at september of 2018 and got the Bachelor's Degree. Nowadays, I'm doing web development  things in both sides which are Frontend and Backend. So, I'm using Reactjs(VanillaJS, TypeScript), Sass and HTML5 for 
+                    I'm {age} years old. I graduated from my university at september of 2018 and got the Bachelor's Degree. Nowadays, I'm doing web development things in both sides which are Frontend and Backend. So, I'm using Reactjs with NextJS(VanillaJS, TypeScript), Sass and HTML5 for 
                     Frontend, Nodejs, Java for Backend. 
                     Former, Huawei engineer.
                 </p>
