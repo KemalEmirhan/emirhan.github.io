ac708bf6b61358b53f546d75a5c5ea38c165b465
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2021-02-10 22:19:06 +0300

    refactor: change files extensions js to jsx

--- components/account/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { accountLinks } from '@constants/index'
-
-export default Account = () => {
-    return (
-        
-    );
-}
---
+++ components/account/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { accountLinks } from '@constants/index'
+
+export default Account = () => {
+    return (
+        <div className="">
+            {Array.from(accountLinks).map((link) => (
+                <>
+                </>
+            ))}
+        </div>
+    );
+}
