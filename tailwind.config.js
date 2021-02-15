6fbf8be986466566b940549daaa5bc2ddd691be2
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2021-02-15 12:43:57 +0300

    chore: re-configure tailwind for prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    darkMode: 'media',
-    theme: {
-        screens: {
-          sm: '480px',
-          md: '768px',
-          lg: '976px',
-          xl: '1440px',
-        },
-        extend: {
-          colors: {
-            'company-blue': '#1e64ff',
-            'dark-background': '#070910',
-            'dark-text-color': '#c9d1d9',
-          }
-        },
-      },
-      variants: {
-        extend: {},
-      },
-      plugins: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        'company-blue': '#1e64ff',
+        'dark-background': '#070910',
+        'dark-text-color': '#c9d1d9',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
