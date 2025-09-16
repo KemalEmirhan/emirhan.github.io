88cc0bfe3edbd28a776be2923195bdad7c48db4a
KemalEmirhan <emirhankemalkosem@gmail.com>
2025-09-16 10:43:06 +0200

    Fix CI issue

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://emirhan.github.io';
+  const baseUrl = 'https://emirkemal.com';
   const currentDate = new Date();
 
   return [
